cbb765ed82ffa99b5dda1433b7b509b1020f775f
Ruen <ruen@khakiout.com>
2020-03-03 23:17:50 +0800

    Add initial project structure

---
+++ src/test/java/com/khakiout/study/pdfgenerator/PdfgeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khakiout.study.pdfgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfgeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
